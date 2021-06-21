702b507c613fe79dae7b82631fbae47d49214bd2
Xeonia <72965305+Xeonia@users.noreply.github.com>
2021-06-21 15:05:37 +0800

    Argparse functionality

--- Server.py
+++ Server.py
@@ -1,12 +1,22 @@
 from scapy.all import *
+import argparse
 
 # These variables can be changed to anything depending on ip of server etc...
-SERVERIP = "192.168.0.2"
-CLIENTIP = "192.168.0.6"
-SERVERMAC = "00:0B:CD:AE:9F:C6"
-CLIENTMAC = "00:02:a5:ea:54:20"
-SUBMASK = "255.255.255.0"
-GATEWAY = "192.168.0.254"
+# SERVERIP = "192.168.0.2"
+# CLIENTIP = "192.168.0.6"
+# SERVERMAC = "00:0B:CD:AE:9F:C6"
+# CLIENTMAC = "00:02:a5:ea:54:20"
+# SUBMASK = "255.255.255.0"
+# GATEWAY = "192.168.0.254"
+
+parser = argparse.ArgumentParser(description="Configure network variables")
+parser.add_argument('ClientIP', type=str, help='Set Client IP')
+parser.add_argument('ServerIP', type=str, help='Set Server IP')
+parser.add_argument('ClientMAC', type=str, help='Set Client MAC')
+parser.add_argument('ServerMAC', type=str, help='Set Server MAC')
+parser.add_argument('SubnetMask', type=str, help='Set Subnet Mask')
+parser.add_argument('DefaultGateway', type=str, help='Set Default Gateway')
+args = parser.parse_args()
 
 
 def detect_dhcp(packet):
@@ -20,18 +30,18 @@ def detect_dhcp(packet):
     if pkt[DHCP] and pkt[DHCP].options[0][1] == 1:
         print("\nReceived DHCP DISCOVER")
 
-        ethernet = Ether(src=SERVERMAC, dst="ff:ff:ff:ff:ff:ff")
-        ip = IP(src=SERVERIP, dst="255.255.255.255")
+        ethernet = Ether(src=args.ServerMAC, dst="ff:ff:ff:ff:ff:ff")
+        ip = IP(src=args.ServerIP, dst="255.255.255.255")
         udp = UDP(sport=67, dport=68)
         bootp = BOOTP(
             op=2,
-            yiaddr=CLIENTIP,
-            siaddr=SERVERIP,
-            giaddr=GATEWAY,
-            chaddr=CLIENTMAC,
+            yiaddr=args.ClientIP,
+            siaddr=args.ServerIP,
+            giaddr=args.DefaultGateway,
+            chaddr=args.ClientMAC,
         )
-        dhcp = DHCP(options=[("message-type", "offer"), ("server_id", SERVERIP), ("broadcast_address",
-                                                                                  "255.255.255.255"), ("router", GATEWAY), ("subnet_mask", SUBMASK)])
+        dhcp = DHCP(options=[("message-type", "offer"), ("server_id", args.ServerIP), ("broadcast_address",
+                                                                                  "255.255.255.255"), ("router", args.DefaultGateway), ("subnet_mask", args.SubnetMask)])
 
         DHCP_OFFER_PKT = ethernet/ip/udp/bootp/dhcp
         sendp(DHCP_OFFER_PKT)
@@ -42,18 +52,18 @@ def detect_dhcp(packet):
     if pkt[DHCP] and pkt[DHCP].options[0][1] == 3:
         print("\nReceived DHCP REQUEST")
 
-        ethernet = Ether(src=SERVERMAC, dst="ff:ff:ff:ff:ff:ff")
-        ip = IP(src=SERVERIP, dst="255.255.255.255")
+        ethernet = Ether(src=args.ServerMAC, dst="ff:ff:ff:ff:ff:ff")
+        ip = IP(src=args.ServerIP, dst="255.255.255.255")
         udp = UDP(sport=67, dport=68)
         bootp = BOOTP(
             op=2,
-            yiaddr=CLIENTIP,
-            siaddr=SERVERIP,
-            giaddr=GATEWAY,
-            chaddr=CLIENTMAC,
+            yiaddr=args.ClientIP,
+            siaddr=args.ServerIP,
+            giaddr=args.DefaultGateway,
+            chaddr=args.ClientMAC,
         )
-        dhcp = DHCP(options=[("message-type", "ack"), ("server_id", SERVERIP), ("broadcast_address",
-                                                                                "255.255.255.255"), ("router", GATEWAY), ("subnet_mask", SUBMASK)])
+        dhcp = DHCP(options=[("message-type", "ack"), ("server_id", args.ServerIP), ("broadcast_address",
+                                                                                "255.255.255.255"), ("router", args.DefaultGateway), ("subnet_mask", args.SubnetMask)])
         DHCP_ACK_PKT = ethernet/ip/udp/bootp/dhcp
         sendp(DHCP_ACK_PKT)
 
